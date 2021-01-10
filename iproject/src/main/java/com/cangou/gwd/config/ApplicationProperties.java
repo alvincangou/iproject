6a5b85a17c4f78db319f65161a2a156025e99685
cangou <cangoualvin@gmail.com>
2021-01-10 18:57:54 -0400

    first commit

---
+++ iproject/src/main/java/com/cangou/gwd/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package com.cangou.gwd.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Iproject.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+}
